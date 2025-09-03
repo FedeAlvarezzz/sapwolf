6ba6b0075d2c37cceb5ed9172da78386a4735fc4
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2025-09-02 22:58:00 -0500

    Configuracion del proyecto

---
+++ src/test/java/co/sapwolf/SapwolfApplicationTests.java
@@ -0,0 +1,13 @@
+package co.sapwolf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapwolfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
